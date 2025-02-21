822bb9a7b85aba305dc346a54ac9bae4becc0a2b
Mikko Keränen <keranemi@icloud.com>
2025-02-21 14:11:18 +0200

    made bottomnav

---
+++ navigation/Navigations.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
+import Antdesign from "react-native-vector-icons/AntDesign";
+import Entypo from "react-native-vector-icons/Entypo";
+import AddLocationScreen from "../screens/AddLocationScreen";
+import LocationsScreen from "../screens/LocationsScreen";
+import MapScreen from "../screens/MapScreen";
+import CapitalsScreen from "../screens/CapitalsScreen";
+
+const Tab = createBottomTabNavigator();
+
+const LOCATIONS = 'Locations';
+const ADDLOCATION = 'Add Location';
+const MAP = 'Map';
+const CAPITALS = 'Capitals';
+
+export default function BottomNav() {
+    return (
+        <NavigationContainer>
+            <Tab.Navigator
+                screenOptions={{
+                    tabBarStyle: {
+                        backgroundColor: '#BBDEFB'
+                    },
+                    animation: 'fade',           
+                }}
+            >
+                <Tab.Screen
+                    name={LOCATIONS}
+                    component={LocationsScreen} 
+                    options={{ tabBarIcon: () => <Entypo name="address" size={24} color="black" /> }} 
+                />
+                <Tab.Screen
+                    name={ADDLOCATION}
+                    component={AddLocationScreen}
+                    options={{ tabBarIcon: () => <Antdesign name="plus" size={26} /> }} 
+                />
+                <Tab.Screen
+                    name={MAP}
+                    component={MapScreen}
+                    options={{ tabBarIcon: () => <Entypo name="map" size={24} color="black" /> }}
+                />
+                <Tab.Screen
+                    name={CAPITALS}
+                    component={CapitalsScreen}
+                    options={{ tabBarIcon: () => <FontAwesome5 name="city" size={24} color="black" /> }}
+                />
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
+}
---
+++ screens/AddLocationScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const AddLocationScreen = () => {
+    return (
+        <View>
+            <Text>AddLocations Screen</Text>
+        </View>
+    );
+};
+
+export default AddLocationScreen;
